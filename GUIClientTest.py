d7fb613bfb3b897b5c28871a94224049263e5a68
Sahil Samar <sahilsamar031@gmail.com>
2023-05-15 16:54:44 -0700

    improvements to client GUI

--- GUIClientTest.py
+++ GUIClientTest.py
@@ -1,88 +1,91 @@
 import tkinter as tk
 import socket
+import threading
+import helper
 
-global serverIP
-global port
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+closing = False
 
 
 def submit():
-    serverIP = str(entry1.get())
-    port = int(entry2.get())
-    result_label.config(text="IP: " + serverIP + "\nPort: " + str(port))
+    ip = helper.get_non_loopback_ip()
+    port = int(port_input.get())
+    result_label.config(text="IP: " + ip + "\nPort: " + str(port))
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((ip, port))
 
-    client.connect((serverIP, port))
+    label_port_input.destroy()
+    port_input.destroy()
+    submit_button.destroy()
 
-    text_box = tk.Text(root, width=50, height=10)
+    text_box = tk.Text(root, width=50, height=10, state="disabled", highlightcolor="red", highlightthickness=2)
     text_box.pack()
 
-    input_entry = tk.Entry(root, width=50)
-    input_entry.pack()
+    frame = tk.Frame(root)
+    frame.pack()
 
+    space = tk.Label(frame, height=1)
+    space.pack()
 
+    input_message_to_send = tk.Entry(root, width=35)
+    input_message_to_send.pack()
 
-    def message():
-        data = input_entry.get()
-        send(data)
+    def receive_message():
+        while not closing:
+            incoming_message = client.recv(1024).decode('utf-8')
+            input_message_to_send.config(state="normal")
+            input_message_to_send.pack()
+            if incoming_message == 'quit':
+                client.close()
+                break
+            else:
+                populate_text("[Server]" + incoming_message + "\n")
 
-    def send(data):
+    def send_message(event=None):
+        data = input_message_to_send.get()
+        populate_text("[Me]" + data + "\n")
+        input_message_to_send.delete(0, tk.END)
         client.send(data.encode('utf-8'))
-        input_entry.delete(0, tk.END)
-        msg = client.recv(1024).decode('utf-8')
-        if msg == 'quit':
-            client.close()
-        else:
-            populate_text("[Server]" + msg + "\n")
-            print("[Server]", msg)
+        input_message_to_send.pack_forget()
+        input_message_to_send.config(state="disabled")
 
-    submit_button = tk.Button(root, text="Submit", command=message)
-    submit_button.pack()
+    input_message_to_send.bind("<Return>", send_message)
+
+    receive_thread = threading.Thread(target=receive_message)
+    receive_thread.start()
 
     def populate_text(data):
+        text_box.config(state="normal")
         text_box.insert(tk.END, data)
-
-    # done = False
-
-    # while not done:
-        #client.send(input("[Me] ").encode('utf-8'))
-        # msg = client.recv(1024).decode('utf-8')
-        # if msg == 'quit':
-        #     done = True
-        # else:
-        #     populate_text("[Server]" + msg + "\n")
-        #     print("[Server]", msg)
-
-    # client.close()
+        text_box.see(tk.END)
+        text_box.config(state="disabled")
 
 
 # Create a Tkinter window
 root = tk.Tk()
 root.title("Chat Client")
 
-# Create the first input box
-label1 = tk.Label(root, text="IP:")
-label1.pack()
+label_port_input = tk.Label(root, text="Port:", justify="center", anchor="center")
+label_port_input.pack()
 
-entry1 = tk.Entry(root, width=50)
-entry1.pack()
+port_input = tk.Entry(root, width=20, justify="center")
+port_input.pack()
 
-# Create the second input box
-label2 = tk.Label(root, text="Port:")
-label2.pack()
-
-entry2 = tk.Entry(root, width=50)
-entry2.pack()
-
-# Create the submit button
-submit_button = tk.Button(root, text="Submit", command=submit)
+submit_button = tk.Button(root, text="Connect", command=submit)
 submit_button.pack()
 
-# Create the result label
 result_label = tk.Label(root, text="")
 result_label.pack()
 
 
+def on_closing():
+    closing = True
+    if client:
+        client.close()  # Close the socket connection if it exists
+    root.destroy()
+
 
 # Start the GUI main loop
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
