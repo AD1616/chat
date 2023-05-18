17f7bd975694f5f9c4ababe15330faa86f5d41ed
Ellen Xu <xu.ellen@outlook.com>
2023-05-18 11:21:48 -0700

    ip from default gateway

--- helper.py
+++ helper.py
@@ -3,16 +3,19 @@ import netifaces
 import socket
 
 def get_non_loopback_ip():
-    interfaces = netifaces.interfaces()
-    for interface in interfaces:
-        if interface == 'lo' or interface.startswith('vbox'):
-            continue
-        addresses = netifaces.ifaddresses(interface)
-        if netifaces.AF_INET in addresses:
-            for address in addresses[netifaces.AF_INET]:
-                ip = address['addr']
-                if not ip.startswith('127.'):
-                    return ip
+    # interfaces = netifaces.interfaces()
+    # for interface in interfaces:
+    #     if interface == 'lo' or interface.startswith('vbox'):
+    #         continue
+    #     addresses = netifaces.ifaddresses(interface)
+    #     if netifaces.AF_INET in addresses:
+    #         for address in addresses[netifaces.AF_INET]:
+    #             ip = address['addr']
+    #             if not ip.startswith('127.'):
+    #                 return ip
+    interface = netifaces.gateways()['default'][netifaces.AF_INET][1] # get name of the "default" gateway interface (e.g. (ip addr, interface name)))
+    ip = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr'] # get ip address associated with interface
+    return ip
 
 
 def extract_port_numbers(ip_address):
